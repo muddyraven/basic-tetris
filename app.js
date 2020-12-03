421bda72bda3c5baa3e058ae90f5d276b0d89447
muddyraven <addcole@gmail.com>
2020-12-03 12:16:25 +0000

    Continued working on drawing the tetrominos in JavaScript

--- app.js
+++ app.js
@@ -44,16 +44,25 @@ document.addEventListener('DOMContentLoaded', () => {
   const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 
   let currentPosition = 4
-  let current = theTetrominoes[0][0]
+  let currentRotation = 0
 
-  //draw the first rotation in the first tetromino
+  //randomly select a Tetromino and its first rotation
+  let random = Math.floor(Math.random()*theTetrominoes.length)
+  let current = theTetrominoes[random][currentRotation]
+
+  //draw a Tetromino
   function draw() {
     current.forEach(index => {
       squares[currentPosition + index].classList.add('tetromino')
     })
   }
 
-  draw()
+  //undraw the Tetromino
+    function undraw() {
+      current.forEach(index => {
+        squares[currentPosition + index].classList.remove('tetromino')
+      })
+    }
 
 
 
